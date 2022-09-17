a0fe02bcc2710f79035d454805de037c2ac68c95
Chowdhury Tanvir Ahmed <CAhmed@spartaglobal.com>
2022-09-18 00:46:47 +0100

    Implementation of Abstract Class

---
+++ Animal/AnimalApp/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalApp
+{
+    public abstract class Animal
+    {
+        string _name;
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+        public string GetName()
+        {
+            return _name;
+        }
+        public abstract void Eat();
+       
+        
+    }
+}
---
+++ Animal/AnimalApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AnimalApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Tobby = new Dog();
+            Tobby.SetName("Tobby");
+            Console.WriteLine(Tobby.GetName());
+            Tobby.Eat();
+        }
+    }
+}
