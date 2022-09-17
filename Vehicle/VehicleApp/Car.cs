84d45d15359a37cc5d8b16eec1a0291ecfcbb7d3
Chowdhury Tanvir Ahmed <CAhmed@spartaglobal.com>
2022-09-17 22:53:05 +0100

    Implementation of Interface

---
+++ Vehicle/VehicleApp/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleApp
+{
+    public class Car : IVehicle
+    {
+        int _amountOfGasoline;
+        public int AmountOfGasoline { get; set; }
+        public Car(int gasoline)
+        {
+            AmountOfGasoline = gasoline;
+        }
+        public void Drive()
+        {
+            if (AmountOfGasoline > 0)
+            {
+                Console.WriteLine("Driving");
+            }
+            else
+            {
+                Console.WriteLine("There is not enough gasoline to start the car");
+            }
+        }
+
+        public bool Refuel(int amountOfGasoline)
+        {
+            AmountOfGasoline += amountOfGasoline;
+            if(AmountOfGasoline > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Vehicle/VehicleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(0);
+            car.Drive();
+            car.Refuel(50);
+            car.Drive();
+        }
+    }
+}
