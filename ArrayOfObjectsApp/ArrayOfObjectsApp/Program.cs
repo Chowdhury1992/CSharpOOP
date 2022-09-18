ae9c93a21f56a11b2dd2d1d7242fa698a1acc089
Chowdhury Tanvir Ahmed <CAhmed@spartaglobal.com>
2022-09-18 01:41:18 +0100

    Creating an Array of objects

---
+++ ArrayOfObjectsApp/ArrayOfObjectsApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace ArrayOfObjectsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Table[] arrayofTable = new Table[10];
+            for(int i =0; i<arrayofTable.Length;i++)
+            {
+                arrayofTable[i] = new Table(rand.Next(50, 201), rand.Next(50, 201));
+                arrayofTable[i].ShowData();
+            }
+        }
+    }
+}
---
+++ ArrayOfObjectsApp/ArrayOfObjectsApp/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOfObjectsApp
+{
+    public class Table
+    {
+        int _width;
+        int _height;
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Table(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine($"Width: {Width}, Height: {Height}");
+        }
+    }
+}
